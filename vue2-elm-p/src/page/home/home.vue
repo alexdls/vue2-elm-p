9131a0ba82f868f41512a745ad4e18a75ae186ed
alex <alex_dls@163.com>
2018-02-09 18:16:40 +0800

    20180209 加入了loading动画，这里是模拟的，之后在请求数据的过程中，就展示这个动画

--- vue2-elm-p/src/page/home/home.vue
+++ vue2-elm-p/src/page/home/home.vue
@@ -1,12 +1,29 @@
 <template>
     <div>
         home页面
+        <loading v-show="showLoading"></loading>
     </div>
 </template>
 
 <script>
+    import loading from '@/components/common/loading'
+
     export default {
-        name: "home"
+        name: "home",
+        data(){
+            return{
+                showLoading: true //加载动画
+            }
+        },
+        components: {
+            loading
+        },
+        mounted() {
+            setTimeout(() => {
+                this.showLoading = false;
+            }, 5000)
+        }
+
     }
 </script>
 
